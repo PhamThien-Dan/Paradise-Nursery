420bc3edf9539902b84b1880d67514d3181d9f32
Phạm Lê Thiên Dấn <phamlethiendan1@gmail.com>
2026-05-28 13:17:00 +0700

    Add AboutUs component for nursery information

---
+++ AboutUs.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+const AboutUs = () => {
+  return (
+    <div className="about-us-container">
+      <h2>About Paradise Nursery</h2>
+      <p>
+        Welcome to Paradise Nursery! We are dedicated to bringing nature into your home 
+        with our wide selection of high-quality, beautiful houseplants. Whether you are 
+        a seasoned plant parent or just starting your green journey, we have the perfect 
+        plant for you.
+      </p>
+    </div>
+  );
+};
+
+export default AboutUs;
